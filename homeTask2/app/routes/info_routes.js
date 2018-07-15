9023158ea469b25b98164d189488d99dadfec579
Marina Arhipova <54321_17@mail.ru>
2018-07-15 14:42:09 +0300

    Practice/homeTask2/

---
+++ homeTask2/app/routes/info_routes.js
@@ -0,0 +1,25 @@
+module.exports = function(app, fs) {
+    app.get('/info', (req, res) => {
+        var url = require('url');
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    fs.appendFile('data.txt', query['lastName'] + ' ' + query['firstName'] + ' ' + query['phoneNumber'] + ' ' + query['progrPit'] + ' ' + query['exampleInputCity'],
+        function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+            res.send();
+        });
+});
+};
+
+/*
+module.exports = function(app, fs) {
+    app.get('/info', (req, res) =>
+    { var url = require('url');
+var url_parts = url.parse(req.url, true);
+var query = url_parts.query;
+fs.appendFile('data.txt', query['name'] + ' ' + query['surname'], + function (err) {
+    if (err) throw err;+ console.log('Saved!');+ res.send();
+});
+});
+}; */
