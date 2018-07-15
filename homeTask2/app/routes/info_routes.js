1780ea927fd17ab8bbe3f033bf2610073ed4a2fc
Marina Arhipova <54321_17@mail.ru>
2018-07-15 15:00:37 +0300

    Practice/homeTask2/

--- homeTask2/app/routes/info_routes.js
+++ homeTask2/app/routes/info_routes.js
@@ -3,7 +3,7 @@ module.exports = function(app, fs) {
         var url = require('url');
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
-    fs.appendFile('data.txt', query['lastName'] + ' ' + query['firstName'] + ' ' + query['phoneNumber'] + ' ' + query['progrPit'] + ' ' + query['exampleInputCity'],
+    fs.appendFile('data.txt', query['lastName'] + ' ' + query['firstName'] + ' ' + query['phoneNumber'] + ' ' + query['progrPit'] + ' ' + query['exampleInputCity']+ ' ' + query['exampleInputStreet']+ ' ' + query['exampleInputHouse']+ ' ' + query['exampleInputStreetFlat'],
         function (err) {
             if (err) throw err;
             console.log('Saved!');
@@ -11,15 +11,3 @@ module.exports = function(app, fs) {
         });
 });
 };
-
-/*
-module.exports = function(app, fs) {
-    app.get('/info', (req, res) =>
-    { var url = require('url');
-var url_parts = url.parse(req.url, true);
-var query = url_parts.query;
-fs.appendFile('data.txt', query['name'] + ' ' + query['surname'], + function (err) {
-    if (err) throw err;+ console.log('Saved!');+ res.send();
-});
-});
-}; */
